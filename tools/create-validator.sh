b000dbbeca75a4cdf5d90f85ced8021941e39e85
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-03-04 00:03:29 +0700

    Update create-validator.sh

--- tools/create-validator.sh
+++ tools/create-validator.sh
@@ -17,20 +17,26 @@ source ~/autonity/.env
 
 # Retrieve private key from oracle key file
 PRIVATE_KEY_ORACLE=$($ETHKEY inspect --private $ORACLE_KEY_FILE <<< "$KEYPASSWORD" | grep "Private key" | awk '{print $3}')
+echo "Private key of oracle retrieved: $PRIVATE_KEY_ORACLE"
 
 # Retrieve treasury account address
 TREASURY_ACCOUNT_ADDRESS=$($AUT account info --keyfile $ORACLE_KEY_FILE | grep "account" | awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
+echo "Treasury account address retrieved: $TREASURY_ACCOUNT_ADDRESS"
 
 # Retrieve ENODE from aut node info output
 ENODE=$($AUT node info | grep "admin_enode" | awk '{print $2}')
+echo "ENODE retrieved: $ENODE"
 
 # Retrieve CONSENSUS_KEY from ethkey autinspect output
 CONSENSUS_KEY=$(/usr/bin/ethkey autinspect ~/autonity-chaindata/autonity/autonitykeys | grep "Consensus Public Key" | awk '{print $4}')
+echo "Consensus key retrieved: $CONSENSUS_KEY"
 
 # Retrieve ORACLE from aut account info output
 ORACLE=$($AUT account info --keyfile /root/.autonity/keystore/oracle.key | grep "account" | awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
+echo "Oracle account retrieved: $ORACLE"
 
 # Run Docker command to register validator and send it to the blockchain
+echo "Running Docker command to register validator and send it to the blockchain..."
 PROOF=$($DOCKER run -t -i \
   --volume $HOME/autonity-chaindata:/autonity-chaindata \
   --volume $HOME/.autonity/keystore/oracle.key:/autoracle/oracle.key \
@@ -41,7 +47,10 @@ PROOF=$($DOCKER run -t -i \
   --autonitykeys /autonity-chaindata/autonity/autonitykeys \
   --oraclekeyhex $PRIVATE_KEY_ORACLE \
   $TREASURY_ACCOUNT_ADDRESS)
+echo "Ownership proof generated: $PROOF"
 
 # Run aut validator register command with PROOF and send it to the blockchain, using passphrase from KEYPASSWORD
+echo "Registering validator and sending it to the blockchain..."
 export 'KEYFILEPWD'="$KEYPASSWORD"
 $AUT validator register $ENODE $ORACLE $CONSENSUS_KEY | $AUT tx sign - | $AUT tx send -
+echo "Validator registered and sent to the blockchain."
