87b7dfe07f3a8f6c1d4f45ce4d0711ac7b626812
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-03-07 23:25:26 +0700

    Update create-validator.sh

--- tools/create-validator.sh
+++ tools/create-validator.sh
@@ -1,62 +1,56 @@
 #!/bin/bash
 
-# Docker Location
+# detect path package
 DOCKER=$(which docker)
-# ethkey Location
 ETHKEY=$(which ethkey)
-
-# aut Location
 AUT=$(which aut)
-
-# Oracle Key File Location
-ORACLE_KEY_FILE=/root/.autonity/keystore/oracle.key
-
-# Treasury Key File Location
-TREASURY_KEY_FILE=/root/.autonity/keystore/treasury.key
 
 # Load password from .env file
 source ~/autonity/.env
 
-# Retrieve private key from oracle key file
+# location oracle.key and treasury.key 
+ORACLE_KEY_FILE=/root/.autonity/keystore/oracle.key
+TREASURY_KEY_FILE=/root/.autonity/keystore/treasury.key
+
+# Get private key from oracle, get treasury account address, get enode, get consensus key, get oracle address 
 PRIVATE_KEY_ORACLE=$($ETHKEY inspect --private $ORACLE_KEY_FILE <<< "$KEYPASSWORD" | grep "Private key" | awk '{print $3}')
-echo "Private key of oracle retrieved: $PRIVATE_KEY_ORACLE"
-
-# Retrieve treasury account address
 TREASURY_ACCOUNT_ADDRESS=$($AUT account info --keyfile $TREASURY_KEY_FILE | grep "account" | awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
-echo "Treasury account address retrieved: $TREASURY_ACCOUNT_ADDRESS"
-
-# Retrieve ENODE from aut node info output
 ENODE=$($AUT node info | grep -o 'enode://[a-zA-Z0-9@.]*:[0-9]*')
-echo "ENODE retrieved: $ENODE"
-
-# Retrieve CONSENSUS_KEY from ethkey autinspect output
 CONSENSUS_KEY=$($ETHKEY autinspect ~/autonity-chaindata/autonity/autonitykeys | grep "Consensus Public Key" | awk '{print $4}')
-echo "Consensus key retrieved: $CONSENSUS_KEY"
-
-# Retrieve ORACLE from aut account info output
 ORACLE=$($AUT account info --keyfile /root/.autonity/keystore/oracle.key | grep "account" | awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
-echo "Oracle account retrieved: $ORACLE"
 
 # Run Docker command to register validator and send it to the blockchain
-echo "Running Docker command to register validator and send it to the blockchain..."
 PROOF=$($DOCKER run -t -i \
-  --volume $HOME/autonity-chaindata:/autonity-chaindata \
-  --volume $HOME/.autonity/keystore/oracle.key:/autoracle/oracle.key \
-  --name proof \
-  --rm \
-  ghcr.io/autonity/autonity:latest \
-  genOwnershipProof \
-  --autonitykeys /autonity-chaindata/autonity/autonitykeys \
-  --oraclekeyhex $PRIVATE_KEY_ORACLE \
-  $TREASURY_ACCOUNT_ADDRESS)
-echo "Ownership proof generated: $PROOF"
+    --volume $HOME/autonity-chaindata:/autonity-chaindata \
+    --volume $HOME/.autonity/keystore/oracle.key:/autoracle/oracle.key \
+    --name proof \
+    --rm \
+    ghcr.io/autonity/autonity:latest \
+    genOwnershipProof \
+    --autonitykeys /autonity-chaindata/autonity/autonitykeys \
+    --oraclekeyhex $PRIVATE_KEY_ORACLE \
+    $TREASURY_ACCOUNT_ADDRESS)
 
-# Clean the proof from non-hexadecimal characters
+# Remove all non-alphanumeric characters from PROOF
 PROOF=$(echo "$PROOF" | tr -cd '[:alnum:]')
-echo "Cleaned ownership proof: $PROOF"
 
 # Run aut validator register command with PROOF and send it to the blockchain, using passphrase from KEYPASSWORD
-echo "Registering validator and sending it to the blockchain..."
 export 'KEYFILEPWD'="$KEYPASSWORD"
 $AUT validator register $ENODE $ORACLE $CONSENSUS_KEY $PROOF | $AUT tx sign - | $AUT tx send -
-echo "Validator registered and sent to the blockchain."
+
+# check validator address
+VALIDATOR_IDENTIFIER_ADDRESS=$($AUT validator compute-address $ENODE | grep -o '0x[a-zA-Z0-9]*')
+
+# check if validator is registered
+VALIDATOR=$($AUT validator list | grep $VALIDATOR_IDENTIFIER_ADDRESS)
+if [ "$VALIDATOR=$VALIDATOR_IDENTIFIER_ADDRESS" ]; then
+    echo "Validator is registered"
+else
+    echo "Validator is not registered"
+fi
+
+echo "your validator address is: $VALIDATOR_IDENTIFIER_ADDRESS"
+
+# save validator address to .autrc file
+echo "validator= $VALIDATOR_IDENTIFIER_ADDRESS" >> ~/.autrc
+echo "to check validator info, run: aut validator info"
