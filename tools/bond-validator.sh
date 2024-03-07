98c7a68e7c46b9307af076b531aacdb105312473
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-03-07 21:59:14 +0700

    Update bond-validator.sh

--- tools/bond-validator.sh
+++ tools/bond-validator.sh
@@ -6,20 +6,43 @@ AUT=$(which aut)
 # Load password from .env file
 source ~/autonity/.env
 
-# Retrieve ENODE from aut node info output
-ENODE=$($AUT node info | grep -o 'enode://[a-zA-Z0-9@.]*:[0-9]*')
-echo "ENODE retrieved: $ENODE"
-
-# Retrieve validator identifier address from enode 
+# Retrieve ENODE and VALIDATOR_IDENTIFIER_ADDRESS
+ENODE=$($AUT node info | grep -o 'enode://[a-zA-Z0-9@.]*:[0-9]*') 
 VALIDATOR_IDENTIFIER_ADDRESS=$AUT validator compute-address $ENODE | grep -o '0x[a-zA-Z0-9]*'
-echo "Validator identifier address retrieved: $VALIDATOR_IDENTIFIER_ADDRESS"
 
-# get account balance from user input
-read -p "Enter the amount NTN to bond: " AMOUNT
-echo "Amount to bond: $AMOUNT"
+# choose to bond or unbond
+echo "Choose to bond or unbond" 1. Bond 2. Unbond
+read -p "Enter choice: " CHOICE
+echo "Choice: $CHOICE"
 
-# Run aut validator bond command
-echo "Running aut validator bond..."
-export 'KEYFILEPWD'="$KEYPASSWORD"
-$AUT validator bond --validator $VALIDATOR_IDENTIFIER_ADDRESS $AMOUNT | $AUT tx sign - | $AUT tx send -
-echo "bond validator completed"
+if [ $CHOICE -eq 1 ]
+then
+    echo "Bonding..."
+elif [ $CHOICE -eq 2 ]
+then
+    echo "Unbonding..."
+else
+    echo "Invalid choice"
+    exit 1
+fi
+
+# retrieve amount to bond or unbond
+read -p "Enter amount: " AMOUNT
+echo "Amount: $AMOUNT"
+
+# Run aut validator bond command or aut validator unbond command
+if [ $CHOICE -eq 1 ]
+then
+    echo "Running aut validator bond..."
+    export 'KEYFILEPWD'="$KEYPASSWORD"
+    $AUT validator bond --validator $VALIDATOR_IDENTIFIER_ADDRESS $AMOUNT | $AUT tx sign - | $AUT tx send -
+    echo "bond validator completed"
+elif [ $CHOICE -eq 2 ]
+then
+    echo "Running aut validator unbond..."
+    export 'KEYFILEPWD'="$KEYPASSWORD"
+    $AUT validator unbond --validator $VALIDATOR_IDENTIFIER_ADDRESS $AMOUNT | $AUT tx sign - | $AUT tx send -
+    echo "unbond validator completed"
+fi
+
+exit 0
