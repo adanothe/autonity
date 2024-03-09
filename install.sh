fb99b4a48239792f3e0b5f59d8ab9089c3aab1f1
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-03-10 00:52:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,29 +3,31 @@
 # Detect Ubuntu version
 UBUNTU_VERSION=$(lsb_release -rs)
 
+AUTONITY=$(which autonity)
+
 # Create a folder for Autonity
 mkdir -p $HOME/.autonity/oracle 
 
 # Check if Docker is already installed
 if ! command -v docker &> /dev/null; then
     # Install Docker
-    curl --proto '=https' --tlsv1.2 -sSfL https://raw.githubusercontent.com/Dedenwrg/dependencies/main/docker/docker.sh | sudo sh >/dev/null 2>&1
+    curl --proto '=https' --tlsv1.2 -sSfL https://raw.githubusercontent.com/Dedenwrg/dependencies/main/docker/docker.sh | sudo sh 
 fi
 
 # Check if Go is already installed
 if ! command -v go &> /dev/null; then
     # Install Go
-    curl --proto '=https' --tlsv1.2 -sSfL https://raw.githubusercontent.com/Dedenwrg/dependencies/main/golang/go.sh | sudo bash >/dev/null 2>&1
+    curl --proto '=https' --tlsv1.2 -sSfL https://raw.githubusercontent.com/Dedenwrg/dependencies/main/golang/go.sh | sudo bash 
 fi
 
 # Install AUT dependencies if not already installed
 if [[ $UBUNTU_VERSION == "20."* ]]; then
     if ! dpkg -s python3-pip python3.8-venv &> /dev/null; then
-        sudo apt install python3-pip python3.8-venv -y >/dev/null 2>&1
+        sudo apt install python3-pip python3.8-venv -y 
     fi
 elif [[ $UBUNTU_VERSION == "22."* ]]; then
     if ! dpkg -s python3-pip python3.10-venv &> /dev/null; then
-        sudo apt install python3-pip python3.10-venv -y >/dev/null 2>&1
+        sudo apt install python3-pip python3.10-venv -y 
     fi
 else
     exit 1
@@ -43,7 +45,7 @@ if ! command -v aut &> /dev/null; then
 fi
 
 # Clone tools repository
-git clone https://github.com/adanothe/autonity.git >/dev/null 2>&1
+git clone https://github.com/adanothe/autonity.git 
 cd autonity
 
 # Export KEYPASSWORD and YOURIP to .env file
@@ -51,8 +53,10 @@ read -p "Enter your KEYPASSWORD: " KEYPASSWORD
 echo "KEYPASSWORD=$KEYPASSWORD" > $HOME/autonity/.env
 echo "YOURIP=$(curl -4 ifconfig.me)" >> $HOME/autonity/.env
 
-# Move ethkey
-sudo cp $HOME/autonity/bin/ethkey /usr/bin/ && sudo chmod +x /usr/bin/ethkey >/dev/null 2>&1
+# Move ethkey & autonity cli
+sudo cp $HOME/autonity/bin/ethkey /usr/bin/ && sudo chmod +x /usr/bin/ethkey 
+sudo cp $HOME/autonity/bin/autonity /usr/bin/ && sudo chmod +x /usr/bin/autonity
+
 
 # Move plugins-conf.yml
 cp $HOME/autonity/plugins/plugins-conf.yml $HOME/.autonity/oracle >/dev/null 2>&1
@@ -63,7 +67,7 @@ chmod +x $HOME/autonity/tools/* >/dev/null 2>&1
 # Create wallet
 $HOME/autonity/tools/create-wallet.sh >/dev/null 2>&1
 echo "your wallet password is $KEYPASSWORD"
-sleep 10
+sleep 5
 
 # Setups .autrc configuration file
 tee <<EOF >/dev/null $HOME/.autrc
@@ -73,10 +77,10 @@ keyfile= ~/.autonity/keystore/treasury.key
 EOF
 
 # start autonity node
-docker compose up -d autonity >/dev/null 2>&1
+$AUTONITY node start >/dev/null 2>&1
 
 # done
 echo  "Installation completed"
 sleep 5
-echo -e "to check node logs: \e[1mdocker logs -f --tail=10 autonity\e[0m"
-echo -e "to check node sync: \e[1maut node info | jq '{eth_blockNumber, eth_syncing}'\e[0m" if false your node is synced
+echo -e "to check node logs: \e[1mautonity node logs\e[0m"
+echo -e "to check node sync: \e[1mautonity node sync\e[0m" if false your node is synced
