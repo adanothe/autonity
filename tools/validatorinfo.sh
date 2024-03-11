0547953475fe38604f12a9a709f953c39c11cbfe
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-03-11 15:46:02 +0700

    Update validatorinfo.sh

--- tools/validatorinfo.sh
+++ tools/validatorinfo.sh
@@ -4,12 +4,12 @@ AUT=$(which aut)
 
 $AUT validator info | jq -r '
   "Treasury address   : \(.treasury)",
-  "node address       : \(.node_address)",
-  "oracle address     : \(.oracle_address)",
-  "enode              : \(.enode)",
-  "bonded stake       : \(.bonded_stake / 1e18 | floor)",
-  "self bonded stake  : \(.self_bonded_stake / 1e18 | floor)",
-  "total slashed      : \(.total_slashed)",
-  "jail release block : \(.jail_release_block)",
-  "validator status   : \(.state | if . == 0 then "active" elif . == 1 then "paused" elif . == 2 then "jailed" else "unknown" end)"
+  "Node address       : \(.node_address)",
+  "Oracle address     : \(.oracle_address)",
+  "Enode              : \(.enode)",
+  "Bonded stake       : \(.bonded_stake / 1e18)",
+  "Self bonded stake  : \(.self_bonded_stake / 1e18)",
+  "Total slashed      : \(.total_slashed)",
+  "Jail release block : \(.jail_release_block)",
+  "Validator status   : \(.state | if . == 0 then "active" elif . == 1 then "paused" elif . == 2 then "jailed" else "unknown" end)"
 '
