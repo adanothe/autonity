6843afe9fb10e3dbc13976a0e7dc69b6ce163d5b
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-03-14 11:11:48 +0700

    Create generate-api.sh

---
+++ tools/cax/generate-api.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source $HOME/autonity/.env
+
+MESSAGE=$(jq -nc --arg nonce "$(date +%s%N)" '$ARGS.named')
+export 'KEYFILEPWD'="$KEYPASSWORD"
+aut account sign-message "$MESSAGE" message.sig
+APIRESPONSE=$(echo -n "$MESSAGE" | https POST "https://cax.piccadilly.autonity.org/api/apikeys" "api-sig:@message.sig")
+NEWAPI=$(echo $APIRESPONSE | jq -r .apikey)
+
+if grep -q "^APIKEY=" "$HOME/autonity/.env"; then
+    
+    sed -i "s#^APIKEY=.*#APIKEY=$NEWAPI#" "$HOME/autonity/.env"
+else
+    
+    echo "APIKEY=$NEWAPI" >> "$HOME/autonity/.env"
+fi
+
+echo "APIKEY added or updated in .env file"
