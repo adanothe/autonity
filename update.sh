228814a95eff152dfa372a92a137b9a5d6f7f46b
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-03-19 11:34:53 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,3 +9,13 @@ sudo cp $HOME/autonity/bin/autonity /usr/bin/ && sudo chmod +x /usr/bin/autonity
 sudo cp -f $HOME/backup/.env $HOME/autonity
 chmod +x $HOME/autonity/tools/*
 chmod +x $HOME/autonity/tools/cax/*
+
+if ! command -v http &> /dev/null; then
+    echo "Installing HTTPie..."
+    curl -SsL https://packages.httpie.io/deb/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/httpie.gpg
+    sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/httpie.gpg] https://packages.httpie.io/deb ./" > /etc/apt/sources.list.d/httpie.list
+    sudo apt update
+    sudo apt install httpie -y
+    echo "HTTPie installed successfully."
+fi
+
