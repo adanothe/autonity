c6194f8b96a936d422cdd2abb7f6036744e80ad3
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-05-13 09:13:54 +0700

    Update wallet-info.sh

--- tools/wallet-info.sh
+++ tools/wallet-info.sh
@@ -6,12 +6,15 @@ find "$DIRECTORY" -type f -name "*.key" -exec sh -c '
     for file; do
         wallet=$(basename "$file" .key) 
         echo "$wallet wallet" 
-        aut account info -k "$file" | jq -r "
-            .[0] | 
-            \"Address      : \(.account)\",
-            \"Balance      : \(.balance)\",
-            \"NTN Balance  : \(.ntn_balance)\"
-        "
+
+        account_info=$(aut account info -k "$file" | jq -r ".[0]")
+        address=$(echo "$account_info" | jq -r ".account")
+        balance=$(echo "$account_info" | jq -r ".balance")
+        ntn_balance=$(echo "$account_info" | jq -r ".ntn_balance")
+
+        echo "Address      : $address"
+        echo "Balance      : $balance"
+        echo "NTN Balance  : $ntn_balance"
         echo "---------------------------------------------------------"
     done
 ' sh {} +
