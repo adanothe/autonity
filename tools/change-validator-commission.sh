1416e38dd1f91a4b322dbffdab68f1ecbd215a67
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-05-17 18:06:25 +0700

    Update change-validator-commission.sh

--- tools/change-validator-commission.sh
+++ tools/change-validator-commission.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-source ~/autonity/.env
+AUT_PATH=$(command -v aut)
 
-AUT=$(which aut)
-ENODE=$($AUT node info | grep -o 'enode://[a-zA-Z0-9@.]*:[0-9]*')
-VALIDATOR_IDENTIFIER_ADDRESS=$($AUT validator compute-address $ENODE | grep -o '0x[a-zA-Z0-9]*')
+if [ -z "$AUT_PATH" ]; then
+    echo "Error: 'aut' command not found. Make sure it is installed and accessible in your PATH."
+    exit 1
+fi
 
-echo "Enter commission rate (in percentage):"
-read -p "Commission rate 1-100: " RATE
-echo "Commission rate: $RATE"
+source ~/autonity/.env || { echo "Error: Unable to source environment variables from ~/autonity/.env"; exit 1; }
 
+ENODE=$("$AUT_PATH" node info | grep -o 'enode://[a-zA-Z0-9@.]*:[0-9]*')
+VALIDATOR_IDENTIFIER_ADDRESS=$("$AUT_PATH" validator compute-address $ENODE | grep -o '0x[a-zA-Z0-9]*')
+
+read -p "Enter commission rate (1-100): " RATE
 COMMISSION_RATE_BPS=$(awk "BEGIN { printf \"%.0f\", $RATE * 100 }")
-
+echo "Commission rate: $RATE%"
 echo "Commission rate in basis points: $COMMISSION_RATE_BPS"
 
+echo "Changing commission rate for validator..."
 export 'KEYFILEPWD'="$KEYPASSWORD"
-$AUT validator change-commission-rate --validator $VALIDATOR_IDENTIFIER_ADDRESS $COMMISSION_RATE_BPS | aut tx sign - | aut tx send -
+"$AUT_PATH" validator change-commission-rate --validator $VALIDATOR_IDENTIFIER_ADDRESS $COMMISSION_RATE_BPS | "$AUT_PATH" tx sign - | "$AUT_PATH" tx send -
+echo "Commission rate change completed."
