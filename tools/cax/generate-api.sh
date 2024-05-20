78cb836a450891b803f75ebae415030c229f52c0
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-05-20 23:06:21 +0700

    add rsponse if wallet not whitelist

--- tools/cax/generate-api.sh
+++ tools/cax/generate-api.sh
@@ -6,13 +6,17 @@ MESSAGE=$(jq -nc --arg nonce "$(date +%s%N)" '$ARGS.named')
 export 'KEYFILEPWD'="$KEYPASSWORD"
 aut account sign-message "$MESSAGE" message.sig
 APIRESPONSE=$(echo -n "$MESSAGE" | https POST "https://cax.piccadilly.autonity.org/api/apikeys" "api-sig:@message.sig")
+
+if [[ $APIRESPONSE == *"Unauthorized"* && $APIRESPONSE == *"Unrecognized signature."* ]]; then
+    echo "Sorry, the wallet you're using cannot create an API key. Action not allowed."
+    exit 1
+fi
+
 NEWAPI=$(echo $APIRESPONSE | jq -r .apikey)
 
 if grep -q "^APIKEY=" "$HOME/autonity/.env"; then
-    
     sed -i "s#^APIKEY=.*#APIKEY=$NEWAPI#" "$HOME/autonity/.env"
 else
-    
     echo "APIKEY=$NEWAPI" >> "$HOME/autonity/.env"
 fi
 
