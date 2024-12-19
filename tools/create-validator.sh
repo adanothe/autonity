6fd3f48f8aef905ea8728c7343f3769a6bebd1f5
dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2024-12-19 17:39:50 +0700

    fix typo & fix crate validator

--- tools/create-validator.sh
+++ tools/create-validator.sh
@@ -6,10 +6,16 @@ echo "Autonity Validator Registration"
 echo "-----------------------------------------------"
 
 ENV_FILE="$HOME/autonity/.env"
+AUTRC_FILE="$HOME/.autrc"
+AUTONITY_KEYS="$HOME/autonity-chaindata/autonity/autonitykeys"
+TREASURY_KEY="$HOME/.autonity/keystore/treasury.key"
+ORACLE_KEY="$HOME/.autonity/keystore/oracle.key"
+DOCKER_IMAGE="ghcr.io/autonity/autonity:latest"
+
 if [ -f "$ENV_FILE" ]; then
     source "$ENV_FILE"
 else
-    echo "Error: env not found at $ENV_FILE"
+    echo "Error: env file not found at $ENV_FILE"
     exit 1
 fi
 
@@ -17,12 +23,8 @@ if [ -z "$KEYPASSWORD" ]; then
     echo "Error: KEYPASSWORD not found in $ENV_FILE"
     exit 1
 fi
-
 export KEYFILEPWD="$KEYPASSWORD"
 
-AUTONITY_KEYS="$HOME/autonity-chaindata/autonity/autonitykeys"
-TREASURY_KEY="$HOME/.autonity/keystore/treasury.key"
-ORACLE_KEY="$HOME/.autonity/keystore/oracle.key"
 TREASURY_ACCOUNT_ADDRESS=$(aut account info --keyfile "$TREASURY_KEY" | jq -r '.[0].account')
 ENODE=$(aut node info | grep -o 'enode://[a-zA-Z0-9@.]*:[0-9]*')
 CONSENSUS_KEY=$(ethkey autinspect "$AUTONITY_KEYS" | awk '/Consensus Public Key/ {print $4}')
@@ -34,16 +36,23 @@ PROOF=$(docker run -t -i \
     --volume "$HOME/.autonity/keystore/oracle.priv:/autoracle/oracle.key" \
     --name proof \
     --rm \
-    ghcr.io/autonity/autonity:latest \
+    $DOCKER_IMAGE \
     genOwnershipProof \
     --autonitykeys /autonity-chaindata/autonity/autonitykeys \
     --oraclekey /autoracle/oracle.key \
     "$TREASURY_ACCOUNT_ADDRESS")
-
 PROOF=$(echo "$PROOF" | tr -cd '[:alnum:]')
+
 TX_HASH=$(aut validator register "$ENODE" "$ORACLE_ADDRESS" "$CONSENSUS_KEY" "$PROOF" | aut tx sign - | aut tx send -)
-VALIDATOR_ADDRESS=$(aut validator info | jq -r '.node_address')
+VALIDATOR_ADDRESS=$(aut validator compute-address "$ENODE")
 VALIDATOR=$(aut validator list | grep "$VALIDATOR_ADDRESS")
+
+if sed -i "s|^validator=.*|validator=$VALIDATOR_ADDRESS|" "$AUTRC_FILE"; then
+    if ! grep -q "^validator=" "$AUTRC_FILE"; then
+        echo "validator=$VALIDATOR_ADDRESS" >> "$AUTRC_FILE"
+    fi
+fi
+
 if [ "$VALIDATOR" == "$VALIDATOR_ADDRESS" ]; then
     echo "Validator registered successfully."
 else
@@ -52,7 +61,6 @@ fi
 
 echo "Transaction hash: https://piccadilly.autonity.org/tx/$TX_HASH"
 echo "Your validator address is: $VALIDATOR_ADDRESS"
-echo "validator=$VALIDATOR_ADDRESS" >>~/.autrc
 echo "To check validator details, run: autonity validator info"
 
 read -p "Press Enter to exit."
